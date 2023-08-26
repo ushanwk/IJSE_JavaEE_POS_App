39a7193b0dd6d8544e38ecc9b20fc71aecf85dae
UshanKaushalya <Ushanwk22@gmail.com>
2023-08-26 10:02:48 +0530

    Item getAll done

--- Back_End/src/lk.ijse/servlet/ItemServlet.java
+++ Back_End/src/lk.ijse/servlet/ItemServlet.java
@@ -1,17 +1,62 @@
 package lk.ijse.servlet;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
 
 @WebServlet(urlPatterns = {"/pages/item"})
 public class ItemServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/JavaeePosApp ", "root", "ushan1234");
+
+            PreparedStatement pstm = connection.prepareStatement("select * from item");
+            ResultSet rst = pstm.executeQuery();
+            PrintWriter writer = resp.getWriter();
+
+            resp.addHeader("Content-Type","application/json");
+            resp.addHeader("Access-Control-Allow-Origin","*");
+
+            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+
+            while (rst.next()) {
+                String code = rst.getString(1);
+                String item = rst.getString(2);
+                String qty = rst.getString(3);
+                String price = rst.getString(4);
+
+                JsonObjectBuilder customer = Json.createObjectBuilder();
+
+                customer.add("customerId", code);
+                customer.add("customerName", item);
+                customer.add("customerAddress", qty);
+                customer.add("customerSalary", price);
+
+                allCustomers.add(customer.build());
+
+            }
+
+            writer.print(allCustomers.build());
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
